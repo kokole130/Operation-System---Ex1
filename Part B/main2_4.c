c730faff0f70799c5d9b4d178f88512167f31a15
yogev100 <yogev2468@gmail.com>
2020-04-05 19:55:24 +0300

    update part b

--- Part B/main2_4.c
+++ Part B/main2_4.c
@@ -1,28 +1,39 @@
+#define _GNU_SOURCE
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <sched.h>
+char process_stack[10000+1];
+
+
+int main2_2(){
+    char* args[]= {"./main2_2",NULL};
+    execvp(args[0],args);
+}
+
 
 int main(){
     pid_t pid = fork();
-    if(pid==0){
-        char* arr1[2]={"./main2_1",NULL};
-        execvp(arr1[0],arr1);
-        pid_t pid = fork();
-        if(pid==0){
-            char* arr2[2]={"./main2_2",NULL};
-            execvp(arr2[0],arr2);
-            pid_t pid = fork(); 
-            if(pid==0){
-                char* arr3[2]={"./main2_3",NULL};
-                execvp(arr3[0],arr3);
-            } 
+    printf("pid is: %d",pid);
+    if(pid==0) {//if this is the child proccess
+        char* args[] = {"./main2_1",NULL};//fork
+        int result = clone(main2_2,process_stack+10000, CLONE_PARENT,0);//clone
+        execvp(args[0],args);
+
+    }
+
+    else{//if this is the parent proccess
+        pid_t pid1 = fork();
+        if(pid1==0) {
+            char* args[] = {"./main2_3",NULL};//daemon
+            execvp(args[0], args);
+        }
+        for (int i = 0; i < 20; ++i) {
+            printf("father process \n");
+            usleep(1000000);
         }
     }
-    //char* arr[]={"./main2_1","./main2_2","./main2_3",NULL};
 
-    // char* arr2[2]={"./main2_2",NULL};
-    // execvp(arr2[0],arr2);
-    // char* arr3[2]={"./main2_3",NULL};
-    // execvp(arr3[0],arr3);
     return 0;
 }
